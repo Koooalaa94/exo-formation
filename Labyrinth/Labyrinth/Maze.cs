6c315f9cdc25e1847574daa360e4e8b32e32acaf
Formation <Formation@DESKTOP-S7S36D3>
2021-09-20 17:50:30 +0200

    rendu final

--- Labyrinth/Labyrinth/Maze.cs
+++ Labyrinth/Labyrinth/Maze.cs
@@ -43,7 +43,7 @@ namespace Labyrinth
 
         public bool IsMazeStart(int i, int j)
         {
-            if (_maze[i,j].Status == 1 )
+            if (_maze[i, j].Status == 1)
             {
                 return true;
             }
@@ -53,7 +53,7 @@ namespace Labyrinth
 
         public bool IsMazeEnd(int i, int j)
         {
-            if (_maze[i,j].Status == -1)
+            if (_maze[i, j].Status == -1)
             {
                 return true;
             }
@@ -68,14 +68,108 @@ namespace Labyrinth
 
         private List<KeyValuePair<int, int>> CloseNeighbors(int i, int j)
         {
-            return null;
+            List<KeyValuePair<int, int>> closeNeighbors = new List<KeyValuePair<int, int>>();
+
+            // Voisin SI case étudié se trouve sur la ligne du dessus
+            if (_maze[i, j].Abscisse == 0)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j + 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j - 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i + 1), j));
+            }
+
+            // Voisin SI case étudié se trouve sur la colonne de gauche
+            if (_maze[i, j].Ordonnee == 0)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j + 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i - 1), j));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i + 1), j));
+            }
+
+            // Voisin SI case étudié se trouve sur la ligne du dessous
+            if (_maze[i, j].Abscisse == _lineSize - 1)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j + 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j - 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i - 1), j));
+            }
+            // Voisin SI case étudié se trouve sur la colonne de droite
+            if (_maze[i, j].Ordonnee == _columnSize - 1)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j - 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i - 1), j));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i + 1), j));
+            }
+
+            // Voisin SI case étudié se trouve dans le coin haut gauche
+            if (_maze[i, j].Abscisse == 0 && _maze[i, j].Ordonnee == 0)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j + 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i + 1), j));
+            }
+
+            // Voisin SI case étudié se trouve dans le coin bas gauche
+            if (_maze[i, j].Abscisse == 0 && _maze[i, j].Ordonnee == _columnSize - 1)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j + 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i - 1), j));
+            }
+
+            // Voisin SI case étudié se trouve dans le coin haut droite
+            if (_maze[i, j].Abscisse == _lineSize - 1 && _maze[i, j].Ordonnee == 0)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j - 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i + 1), j));
+            }
+
+            // Voisin SI case étudié se trouve dans le coin bas droite
+            if (_maze[i, j].Abscisse == _lineSize - 1 && _maze[i, j].Ordonnee == _columnSize - 1)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j - 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i - 1), j));
+            }
+
+            // Voisin SI case étudié ne se trouve sur aucune extrémité
+            if (_maze[i, j].Abscisse == 0 || _maze[i, j].Ordonnee == 0 || _maze[i, j].Abscisse == _lineSize - 1 || _maze[i, j].Ordonnee == _columnSize - 1)
+            {
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j + 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>(i, (j - 1)));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i + 1), j));
+                closeNeighbors.Add(new KeyValuePair<int, int>((i - 1), j));
+            }
+            return closeNeighbors;
         }
+
 
         public KeyValuePair<int, int> Generate()
         {
+            Random r = new Random();
+            KeyValuePair<int, int> cellule = new KeyValuePair<int, int>();
+            cellule<int, int> = r.Next(_lineSize, _columnSize);
+            _maze[i,j].IsVisited = true;
+
             return new KeyValuePair<int, int>();
+
+            // 5.b. Toutes les cases du labyrinth seront visité car pour chaque case il visite la case voisine puis retourne en arrière et réitère la manoeuvre, toute les cellule seront ainsi visité jusqua la cellule sortie. 
         }
 
+        /* 6.
+         * │ = parois gauche/droite fermé
+         * ─ = parois haut/bas fermé
+         * ├ = double parois vertical fermé avec début d'angle droite
+         * ┼ = double parois vertical fermé avec d'but d'angle droite et gauche
+         * ┤ = double parois vertical fermé avec début d'angle gauche
+         * ' ' = parois ouverte
+         * ╷ = simple parois vertical
+         * ┐ = simple parois vertical avec angle haut gauche
+         * ┌ = simple parois vertical avec angle haut droite
+         * ┬ = simple parois vertical avec angle haut droite et gauche
+         * ┘ = simple parois vertical avec angle bas gauche
+         * └ = simple parois vertical avec angle bas droite
+         * ┴ = simple parois vertical avec angle bas droite et gauche
+         * ╶ = demi parois horizontal droite (parois fermé avec angle droite)
+         * ╴ = demi parois horizontal gauche (parois fermé avec angle gauche)
+         */
         public string DisplayLine(int n)
         {
             return string.Empty;
